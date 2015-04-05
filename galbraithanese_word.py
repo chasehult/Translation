6a78f5c59d313628a422acd836d4bf24aeb0d434
Chase Hult <kjellhult@Study-iMac.local>
2015-04-05 16:34:05 -0700

    asdf

--- galbraithanese_word.py
+++ galbraithanese_word.py
@@ -321,6 +321,118 @@ class Translation:
                     return eng+"s"
             return "?"*len(word)
 
+
+    def fulltranslate(self, sentence):
+        def trymost(word):
+            try:
+                return gettranslation(word)
+            except:
+                return False
+        def trynoun(word):
+            if word[-3:]=="ób":
+                case="nom"
+                word=word[:-3]
+            elif word[-5:]=="îgē":
+                case="gen"
+                word=word[:-5]
+            elif word[-5:]=="ûbē":
+                case="dat"
+                word=word[:-5]
+            elif word[-5:]=="ōbō":
+                case="acc"
+                word=word[:-5]
+            elif word[-5:]=="óló":
+                case="abl"
+                word=word[:-5]
+            else:
+                return False
+            if word[-4:]=="nôb":
+                num="non"
+                word=word[:-4]
+            elif word[-5:]=="trôb":
+                num="sin"
+                word=word[:-5]
+            elif word[-5:]=="flôb":
+                num="bil"
+                word=word[:-5]
+            elif word[-5:]=="glôb":
+                num="plu"
+                word=word[:-5]
+            else:
+                return False
+            prefix=""
+            suffix=""
+            try:
+                if case=="gen":
+                    prefix="of the "
+                if num in ["non", "bil", "plu"]:
+                    suffix="s"
+                return prefix+self.gettranslation(word)+suffix
+            except:
+                return False
+        def tryadjective(word):
+            if word[-4:]=="əʃ":
+                return tryadjective(word[:-4])+"ly"
+            try:
+                return self.gettranslation(word)
+            except:
+                return False
+        def tryadverb(word):
+            if word[-4:]=="óθ":
+                return trynoun(word[:-4])+"ful"
+            try:
+                return self.gettranslation(word)
+            except:
+                return False
+        def tryverb(word):
+            if word[-3:]=="ôk":
+                try:
+                    return "y'all need to "+self.gettranslation(word) if word[-6:]=="īpôk" else "you need to "+self.gettranslation(word)
+                except:
+                    return False
+            if word[-2:]=="ó":
+                try:
+                    return "to "+self.gettranslation(word)
+                except:
+                    return False
+            pro=False
+            if word[-5:]=="îgē":
+                pro=True
+                word=word[:-5]
+            if word[-5:]=="ēnē":
+                tense="pas"
+                word=word[:-5]
+            elif word[-3:]=="ûb":
+                tense="pre"
+                word=word[:-3]
+            elif word[-4:]=="ûgl":
+                tense="fut"
+                word=word[:-4]
+            else:
+                return False
+            plu=False
+            if word[-3:]=="īp":
+                plu=True
+                word=word[:-3]
+            if word[-6:]=="ópóp":
+                person=1
+                word=word[:-6]
+            elif word[-6:]=="ôbēb":
+                person=2
+                word=word[:-6]
+            elif word[-3:]=="óf":
+                person=3
+                word=word[:-3]
+            else:
+                person=4
+            perstr=[None, "we", "y'all", "they", "everyone"][person] if plu else: [None, "I", "you", "they", "anyone"][person]
+            try:
+                ver=self.gettranslation(word)
+            
+                
+            
+            
+    
     def custom_trans(self, word, trans):
         self.dictionary[word]=trans
 
@@ -391,8 +503,161 @@ class Translation:
         if newword[-1]==u"-":
             newword=newword[:-1]
         return newword
-    
 
+    def fullsentence(self, sentence):
+        def askquestion(question, answers):
+            newanswers=""
+            for x in range(len(answers)):
+                newanswers+="\t"+str(x+1)+". "+answers[x]+"\n"
+            x="asdf"
+            while x not in map(lambda x: str(x+1), range(len(answers))):
+                if x!="asdf":
+                    print "Invalid answer"
+                x=raw_input(question+"\n"+newanswers)
+            return int(x)
+            
+        newsentence=""
+        for letter in sentence.lower():
+            if letter not in ",?.!:;":
+                newsentence+=letter
+        output=[]
+        pro=""
+        lastisto=False
+        for word in newsentence.split():
+            if word=="to":
+                lastisto=True
+                continue
+            x=askquestion("The word \""+word+"\" is:", ["An article", "A noun", "An adjective", "A verb", "An adverb", "A conjunction", "A preposition", "An interjection", "A pronoun"])
+            if x==1:  #Article
+                pass   #Do nothing
+            elif x==2:  #Noun
+                x=askquestion("There are/is ___ of this noun (\""+word+"\"):", ["None", "One", "Two", "None of the above"])
+                if x==1:  #Nonullar
+                    number="nôb"
+                elif x==2:  #Singular
+                    number="trôb"
+                elif x==3:  #Bilar
+                    number="flôb"
+                elif x==4:  #Plural
+                    number="glôb"
+                x=askquestion("The noun (\""+word+"\") is in the ___ case:", ["Nominative", "Genitive", "Dative", "Accusitive", "Ablative"])
+                if x==1:  #Nom
+                    case="ób"
+                elif x==2:  #Gen
+                    case="îgē"
+                elif x==3:  #Dat
+                    case="ûbē"
+                elif x==4:  #Acc
+                    case="ōbō"
+                elif x==5:  #Abl
+                    case="óló"
+                works=False
+                while not works:
+                    x=raw_input("Please enter a simple form of the noun \""+word+"\" (ex. of the cats -> cat)")
+                    try:
+                        output.append(self.getword(x)+number+case)
+                        works=True
+                    except:
+                        print "Even simpler"
+            elif x==3:  #Adjective
+                x=askquestion("Is the adjective \""+word+"\" formed from a noun?", ["Yes", "No"])
+                if x==1:
+                    works=False
+                    while not works:
+                        x=raw_input("Please enter a simple form of the noun that makes up the adjective \""+word+"\" (ex. windy -> wind)")
+                        try:
+                            output.append(self.getword(x)+"óθ")
+                            works=True
+                        except:
+                            print "Even simpler"
+                else:
+                    output.append(self.getword(x))
+            elif x==4:  #Verb
+                isinf=False
+                if lastisto:
+                    x=askquestion("Is \"to "+word+"\" a full infinitive/to-infinitive?", ["Yes", "No"])
+                    if x==1:
+                        works=False
+                        while not works:
+                            x=raw_input("Please enter a simple form of the infinitive \""+word+"\" (ex. to run -> run)")
+                            try:
+                                isinf=True
+                                output.append(self.getword(x)+"ó")
+                                works=True
+                            except:
+                                print "Even simpler"
+                    elif x==2:
+                        output.append("to")
+                isimp=True
+                if not isinf:
+                    x=askquestion("Is the verb \""+pro+word+"\" imperative?", ["Yes", "No"])
+                    if x==1:
+                        plural=["","","īp"][askquestion("Is the imperative verb \""+pro+word+"\" plural?", ["Yes", "No"])]
+                        works=False
+                        while not works:
+                            x=raw_input("Please enter a simple form of the imperative \""+pro+word+"\" (ex. All of you, run! -> run)")
+                            try:
+                                output.append(self.getword(x)+plural+"ôk")
+                                works=True
+                            except:
+                                print "Even simpler"
+                    elif x==2:
+                        isimp=False
+                if not isimp and not isinf:
+                    person=["","ópóp","ôbēb","óf"][askquestion("What person is the verb \""+pro+word+"\" in?", ["1st", "2nd", "3rd"])]
+                    plural=["","īp",""][askquestion("Is the verb \""+pro+word+"\" plural?", ["Yes", "No"])]
+                    tense=["","ēnē","ûb","ûgl"][askquestion("What tense is the verb \""+pro+word+"\" in?", ["Past", "Present", "Future"])]
+                    progress=["","îgē",""][askquestion("Is the verb \""+pro+word+"\" progressive?", ["Yes", "No"])]
+                    works=False
+                    while not works:
+                        x=raw_input("Please enter a simple form of the verb \""+pro+word+"\" (ex. I was running -> run)")
+                        try:
+                            output.append(self.getword(x)+person+plural+tense+progress)
+                            works=True
+                        except:
+                            print "Even simpler"
+                pro=""
+            elif x==5:  #Adverb
+                x=askquestion("Is the adverb \""+word+"\" an adjective with -ly or something added at the end?", ["Yes", "No"])
+                if x==1:
+                    works=False
+                    while not works:
+                        x=raw_input("Please enter a simple form of the adjective that makes up the adverb \""+word+"\" (ex. quickly -> quick)")
+                        try:
+                            output.append(self.getword(x)+"əʃ")
+                            works=True
+                        except:
+                            print "Even simpler"
+                else:
+                    output.append(self.getword(word))
+            elif x==6: #Conjunction
+                output.append(self.getword(word))
+            elif x==7: #Preposition
+                output.append(self.getword(word))
+            elif x==8: #Interjection
+                output.append(self.getword(word))
+            elif x==9: #Pronoun
+                pro=word+" "
+                continue
+            if pro:
+                output.insert(len(output)-1, pro[:-1])
+                pro=""
+            if lastisto:
+                output.insert(len(output)-1, self.getword("to"))
+        return " ".join(output)
+                
+                
+                    
+                
+    
+                
+        
+                
+        
+
+r="əʃ"
+    
+# ksīʧūmópópûb tîkl plərôðó tîkl klûʃîmēsp tîkl klênōkrīθ tîkl slētróᵲūbr = I like to play games with people
     
         
 
